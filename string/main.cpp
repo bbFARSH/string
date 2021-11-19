1baab7f1772b71ab03ebb8ce43413ff576bacf49
Sali_dk52@itstepru.onmicrosoft.com <Sali_dk52@itstepru.onmicrosoft.com>
2021-11-19 20:02:24 +0300

    METHODSOUTOFCLASS

--- string/main.cpp
+++ string/main.cpp
@@ -4,6 +4,9 @@ using std::cin;
 using std::cout;
 using std::endl;
 class String;
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////Объявление класса - CLass Declaration////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 String operator+(const String& left, const String& right);
 
 class String
@@ -12,95 +15,116 @@ class String
 	char* str; 
 public:
 	//	Constructor
-	explicit String(int size = 80) :size(size), str(new char[size] {})
-	{
-		//this->size = size;
-		//this->str = new char[size] {};
-		cout << "DefaultConstructor:\t" << this << endl;
-	}
-	String(const char* str) : String(strlen(str)+1) // Делегируем выделение памяти конструктору по умолчанию
-	{
-		//this->size = strlen(str) + 1;
-		//this->str = new char[size] {};
-		for (int i = 0; i < size; i++)this->str[i] = str[i];
-		cout << "Constructor:\t\t" << this << endl;
-	}
-	String(const String& other):String(other.str)
-	{
-		//this->size = other.size;
-		//this->str = new char[size] {};
-		//for (int i = 0; i < size; i++)this->str[i] = other.str[i];
-		cout << "CopyConstructor:\t" << this << endl;
-	}
-	String(String&& other): size(other.size), str(other.str)
-	{
-		//shallow copy
-		//move-конструктор должен работать так, как не должен работать конструктор копирования(диаметральная противоположность)
-		//this->size = other.size;
-		//this->str = other.str;
-		cout << "MoveConstructor:\t\t" << this << endl;
-		other.str = nullptr;
-		other.size = 0;
-	}
-	~String()
-	{
-		delete[] this->str;
-		cout << "Destructor:\t\t" << this << endl;
-	}
-
+	explicit String(int size = 80);
+	String(const char* str);
+	String(const String& other);
+	String(String&& other);
+	~String();
 	//			Operators:
-	String& operator=(const String& other)
-	{
-		if (this == &other)return *this;
-		delete[] this->str;
-		this->size = other.size;
-		this->str = new char[size] {};
-		for (int i = 0; i < size; i++)this->str[i] = other.str[i];
-		cout << "CopyAssigment:\t\t" << this << endl;
-		return *this;
-	}
-	String& opearator(String&& other)
-	{
-		this->size = other.size;
-		this->str = other.str;
-		other.str = nullptr;
-		other.size;
-		return *this;
-	}
-	String operator+=(const String& other)
-	{
-		return *this = *this + other;
-	}
+	String& operator=(const String& other);
+	String& opearator(String&& other);
+	String operator+=(const String& other);
+	void print()const;
 
-	void print()const
-	{
-		cout << "Size:\t" << size << endl;
-		cout << "Str:\t" << str << endl;
-	}
-
-	int get_size()const
-	{
-		return size;
-	}
-	const char* get_str()const
-	{
-		return str;
-	}
-	 char* get_str()
-	{
-		return str;
-	}
-	 const char &operator[](int i)const
-	 {
-		 return str[i];
-	 }
-	 char& operator[](int i)
-	 {
-		 return str[i];
-	 }
+	int get_size()const;
+	const char* get_str()const;
+	char* get_str();
+	 const char& operator[](int i)const;
+	 char& operator[](int i);
 
 };
-String operator+(const String& left, const String& right)
+/////////////////// КОНЕЦ ОБЪЯВЛЕНИЯ КЛАССА //////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+///////////////////ОПРЕДЕЛЕНИЕ КЛАССА - CLASS DEFINITION       ////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+String::String(int size) :size(size), str(new char[size] {})
+{
+	//this->size = size;
+	//this->str = new char[size] {};
+	cout << "DefaultConstructor:\t" << this << endl;
+}
+String::String(const char* str) : String(strlen(str) + 1) // Делегируем выделение памяти конструктору по умолчанию
+{
+	//this->size = strlen(str) + 1;
+	//this->str = new char[size] {};
+	for (int i = 0; i < size; i++)this->str[i] = str[i];
+	cout << "Constructor:\t\t" << this << endl;
+}
+String::String(const String& other) :String(other.str)
+{
+	//this->size = other.size;
+	//this->str = new char[size] {};
+	//for (int i = 0; i < size; i++)this->str[i] = other.str[i];
+	cout << "CopyConstructor:\t" << this << endl;
+}
+String::String(String&& other) : size(other.size), str(other.str)
+{
+	//shallow copy
+	//move-конструктор должен работать так, как не должен работать конструктор копирования(диаметральная противоположность)
+	//this->size = other.size;
+	//this->str = other.str;
+	cout << "MoveConstructor:\t\t" << this << endl;
+	other.str = nullptr;
+	other.size = 0;
+}
+String::~String()
+{
+	delete[] this->str;
+	cout << "Destructor:\t\t" << this << endl;
+}
+
+//			Operators:
+String& String::operator=(const String& other)
+{
+	if (this == &other)return *this;
+	delete[] this->str;
+	this->size = other.size;
+	this->str = new char[size] {};
+	for (int i = 0; i < size; i++)this->str[i] = other.str[i];
+	cout << "CopyAssigment:\t\t" << this << endl;
+	return *this;
+}
+String& String::opearator(String&& other)
+{
+	this->size = other.size;
+	this->str = other.str;
+	other.str = nullptr;
+	other.size;
+	return *this;
+}
+String String::operator+=(const String& other)
+{
+	return *this = *this + other;
+}
+
+void String::print()const
+{
+	cout << "Size:\t" << size << endl;
+	cout << "Str:\t" << str << endl;
+}
+
+int String::get_size()const
+{
+	return size;
+}
+const char* String::get_str()const
+{
+	return str;
+}
+char* String::get_str()
+{
+	return str;
+}
+const char& String::operator[](int i)const
+{
+	return str[i];
+}
+char& String::operator[](int i)
+{
+	return str[i];
+}
+::String operator+(const String& left, const String& right)
 {
 	String buffer(left.get_size() + right.get_size() - 1);
 	for (int i = 0; i < left.get_size(); i++)
@@ -109,11 +133,12 @@ String operator+(const String& left, const String& right)
 		buffer[i + left.get_size() - 1] = right[i];
 	return buffer;
 }
-ostream& operator<<(ostream& os, const String& obj)
+std::ostream& operator<<(std::ostream& os, const String& obj)
 {
 	return os << obj.get_str();
 }
-
+/////////////////// КОНЕЦ ОПРЕДЕЛЕНИЯ КЛАСССА //////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //#define CONSTRUCTOR_CHECK
 //#define operator_plus_check
 void main()
